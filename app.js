ffc6b514f9ead911f7925593e441d97aae28640e
SreeAnanthaKannan <sreeananthakannan@gmail.com>
2020-01-23 01:10:38 +0530

    added

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express();
+
+app.get( '/', (req, res) => {
+    console.log('called updated')
+    res.send('helo world haha ')
+})
+
+app.listen( 3000, () =>{
+    console.log(`my app runign in port 3000`)
+});
+
+// docker build -t node-docker .
+// docker run -it -p 9000:3000 node-docker
